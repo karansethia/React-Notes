d60d29b03f254e3289987008c267c6f7234585dc
Karan <karansethia24@gmail.com>
2023-09-03 22:05:20 +0530

    Changes: Navbar Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import NoteDash from "./User/Pages/NoteDash";
+import Navbar from "./User/Components/Navbar/Navbar";
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <NoteDash />
     </div>
   );
