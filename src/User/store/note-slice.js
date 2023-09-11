bf9685bc9e14f6960996b8476519403b20ce6e50
Karan <karansethia24@gmail.com>
2023-09-11 23:58:38 +0530

    Changes: Redux store structure

---
+++ src/User/store/note-slice.js
@@ -0,0 +1,9 @@
+/* Note Structure
+  note: [
+    {id: 'note1',
+    title: 'This is note title',
+    content: 'This is note content',
+    includedtags: [{tagName: 'work', color: 'red'},{},{}],
+    isPin: true}
+  ]
+*/
