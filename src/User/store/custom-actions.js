841413bf774c845c2498e13acfadb344694c0694
Karan <karansethia24@gmail.com>
2023-09-22 01:17:46 +0530

    Changes: UI slice completed

---
+++ src/User/store/custom-actions.js
@@ -0,0 +1,43 @@
+import { uiActions } from "./ui-slice";
+
+export const setLogOut = () => {
+  return async(dispatch)=> {
+    const setLog = async() => {
+      const isLoggedIn = localStorage.getItem('isLoggenIn');
+      if(isLoggedIn){
+        localStorage.setItem('isLgged',false);
+        return "Succesfully Logged Out"
+      }
+      return "You gotta login first man"
+    }
+    try{
+      const notification = await setLog();
+      dispatch(
+        uiActions.showModal(notification)
+      )
+    }catch(err){
+      dispatch(
+        uiActions.showModal("An error occured while logging out")
+      )
+    }
+  }
+}
+
+export const setLogIn = () => {
+  return async(dispatch) =>{
+    const setLog = async() => {
+      localStorage.setItem('isLoggedIn',true);
+      return "Logged In succesfully";
+    }
+    try{
+      const notification = await setLog();
+      dispatch(
+        uiActions.showModal(notification)
+      )
+    }catch(err){
+      dispatch(
+        uiActions.showModal(notification)
+      )
+    }
+  }
+}
--- src/User/store/ui-slice.js
+++ src/User/store/ui-slice.js
@@ -16,19 +16,26 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const uiSlice = createSlice({
   name : "UI",
-  initialState: {darkTheme:true,isLoggedIn:true,sortRecent: true, showTag: null},
+  initialState: {darkTheme:true,isLoggedIn:true,sortRecent: true, showTag: null,modalInfo:{modelMessage:null,showModal:true}},
   reducers: {
     changeTheme(state){
       //!set the theme to !darkTheme
+      state.darkTheme = !state.darkTheme
     },
     logout(state){
       //! set isLoggedIn to false
+      state.isLoggedIn(false);
     },
     sortByRecent(state){
       //! set sortRecent to !sortRecent
+      state.sortRecent = !state.sortRecent;
     },
     sortTag(state, action){
-
+      state.sortTag = action.payload
+    },
+    showModal(state){
+      state.modalInfo.modelMessage = action.payload;
+      state.modalInfo.showModal = true;
     }
   }
 });
