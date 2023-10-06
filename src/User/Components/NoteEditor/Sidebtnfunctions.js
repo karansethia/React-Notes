6828ab2bc1913861afabb91e7848f66d647d8db5
Karan <karansethia24@gmail.com>
2023-10-06 23:55:51 +0530

    Changes: Sidebar UI seperated

---
+++ src/User/Components/NoteEditor/Sidebtnfunctions.js
@@ -0,0 +1,15 @@
+export const saveHandler = () => {
+  console.log('sidebtn clicked : save')
+}
+export const copyHandler = () => {
+  console.log('sidebtn clicked : copy')
+}
+export const exportHandler = () => {
+  console.log('sidebtn clicked : export')
+}
+export const deleteHandler = () => {
+  console.log('sidebtn clicked : delete')
+}
+export const pinHandler = () => {
+  console.log('sidebtn clicked : pin')
+}
