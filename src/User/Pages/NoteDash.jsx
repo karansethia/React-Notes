2c42d1f7c5fff843d167d6e44a103044fa28cf3d
Karan <karansethia24@gmail.com>
2024-02-13 12:38:41 +0530

    Update: Reverting to plain react

--- src/User/Pages/NoteDash.jsx
+++ src/User/Pages/NoteDash.jsx
@@ -1,29 +1,10 @@
 import React, {useEffect} from "react";
-import {noteActions} from "../store/note-slice";
 import classes from "./NoteDash.module.css";
-import {useQuery} from "@tanstack/react-query";
 import Notelist from "../Components/NoteList/Notelist";
 import Tagbar from "../Components/Tagbar/Tagbar";
-import {useDispatch, useSelector} from "react-redux";
-import {postLogin} from "../../utils/http";
+import {useSelector} from "react-redux";
 
 const NoteDash = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const {data, isError, error, isLoading} = useQuery({
-      queryKey: ["notes"],
-      queryFn: postLogin,
-    });
-    console.log(data);
-    if (!isLoading && !isError) {
-      dispatch(noteActions.addNotes(data));
-    }
-    if (isError || error) {
-      //todo dispatch notification
-    }
-  });
-
   const notes = useSelector((state) => state.stateNotes.notes);
   // console.log(data);
 
@@ -31,7 +12,7 @@ const NoteDash = () => {
     <div className={classes.container}>
       <div className={classes.notedash}>
         <Tagbar />
-        {!isLoading && <Notelist notes={notes} />}
+        <Notelist notes={notes} />
       </div>
     </div>
   );
